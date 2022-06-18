fe8194acfb7dc4d2f47decc37de4e90d6a0f38a7
wilczeu <105659485+wilczeu@users.noreply.github.com>
2022-06-18 22:03:54 +0200

    ver 1.3
    In this version:
    - counting how many times you have pressed the "bored button"
    - after first click, showing text "LoL" (because why not, LoL)

--- iambored-mobile/main.py
+++ iambored-mobile/main.py
@@ -11,13 +11,29 @@ import colorsys
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.floatlayout import FloatLayout
 
+bttn = Button()
+
+clicks = 0
 
 class MyGrid(Widget):
+    def bored_bttn(self):
+        self.ids.B01.text = "And you're still bored..?"
+        self.B01 = Button()
+        self.ids.L2.text = "LoL"
+        self.B01.bind(on_press=self.bored_bttn)
+        global clicks
+        clicks += 1
+        self.ids.L1.text = f"U r bored x {clicks}"
+
+
     pass
 
 class Bored(App):
     def build(self):
+
+
         return MyGrid()
+    
 
 if __name__ == "__main__":
     Bored().run()    
